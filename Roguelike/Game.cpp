1028eac68fae28886ec0906b67752fca8eec8ef1
Elaviers <elaviers@gmail.com>
2016-10-15 22:37:21 -0700

    Made the game actually render something

--- Roguelike/Game.cpp
+++ Roguelike/Game.cpp
@@ -1,19 +1,52 @@
 #include "Game.h"
 
-Game::Game() : _running(true)
-{
-}
+Game::Game() : _running(true) {}
 
 void Game::start() {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	_window.create("The Window of Hope", 1280, 720, 0);
+
+	SDL_GL_SetSwapInterval(0);
+
+	std::printf("You're still running OpenGL version %s? What a noob!\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	//////////////////init shaders
+	_shader.compileShaders("Shaders/DRUGS.frag", "Shaders/DRUGS.vert");
+	_shader.addAttribute("vertPosition");
+	_shader.addAttribute("vertColour");
+	_shader.addAttribute("vertUV");
+	_shader.linkShaders();
+	/////////////////
+
+	_sprite.init(-1, -1, 2, 2);
 	loop();
 }
 
 void Game::loop() {
+	int frameNumber = 0;
 	while (_running) {
+		frameNumber++;
+
+		_frameTimer.begin();
 		handleInput();
+		render();
+		time += _frameTimer.deltaTime;
+		_frameTimer.end();
+
+		std::printf("%f\n",time);
+
+		if (frameNumber % 10 == 0)_window.setTitle("The phsychedelic window of hope, running at a buttery smooth "+std::to_string(_frameTimer.getFramerate())+" frames a second!");
 	}
+}
+
+void Game::render() {
+	_shader.useProgram();
+	GLint shaderTime = _shader.getUniformLocation("time");
+	glUniform1f(shaderTime, time);
+	_sprite.render();
+	_shader.unUseProgram();
+
+	_window.swapBuffer();
 }
 
 void Game::handleInput() {
--- Roguelike/Game.h
+++ Roguelike/Game.h
@@ -2,17 +2,30 @@
 
 #include <SDL/SDL.h>
 #include <Engine/Window.h>
+#include <Engine/Timing.h>
+#include <Engine/GLSLShading.h>
+#include <Engine/Sprite.h>
 
 class Game
 {
 private:	
 	Window _window;
+	Timing _frameTimer;
 	bool _running;
+
+	Sprite _sprite;
+	GLSLShading _shader;
 public:
+	float time;
+
+
 	Game();
 
 	void start();
 	void loop();
+
+	void render();
+
 	void handleInput();
 };
 
